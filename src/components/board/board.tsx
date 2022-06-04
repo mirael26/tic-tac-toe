7a86c5f45bd2e10b50453761c46aef8122c68452
Elmira Ilyasova <capricorn_26@mail.ru>
2022-06-04 17:00:00 +0700

    Set cells displaying

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
+
+import {CellValue} from "../../types";
+
 import Cell from "../cell/cell";
+
+const Cells: Array<CellValue> = [null, 0, 1, null, 1, null, 1, null, null];
 
 const Board = ():JSX.Element => {
   return (
     <div className="board">
-      <Cell />
+      {Cells.map((el, id) => {
+        return <Cell key={`cell-${id}`} value={el}/>
+      })}
       <img className="board__image" src={require("../../img/board.png")} alt="game board"/>
     </div>
   );
--- src/components/cell/cell.tsx
+++ src/components/cell/cell.tsx
@@ -1,9 +1,30 @@
 import * as React from "react";
 
-const Cell = ():JSX.Element => {
+import { CellValue } from "../../types";
+
+const CellImage = {
+  CROSS: require("../../img/cross.png"),
+  ZERO: require("../../img/zero.png")
+}
+
+interface CellProps {
+  value: CellValue
+}
+
+
+
+const Cell = ({value}: CellProps): JSX.Element => {
+  let cellImage;
+  if (value !== null) {
+    cellImage = (value === 1) ? CellImage.CROSS : CellImage.ZERO;
+  }
+
   return (
     <div className="cell">
-      
+      {value !== null && cellImage
+        ? <img className="cell__image" src={cellImage} alt="ячейка" />
+        : null
+      }
     </div>
   );
 }
